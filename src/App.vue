4167fa00903d83f9f26ff2b4008e28ea0de1b0fb
andre-brdoch <andrebrdoch@yhahoo.de>
2023-03-09 10:19:48 +0100

    feat: Add AppProgrammatic component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <AppPerson first-name="Max" last-name="Mustermann" :age="33" />
+    <AppPerson first-name="Max" last-name="Mustermann" :age="33" />
+    <AppProgrammatic />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AppPerson from './components/AppPerson/AppPerson.vue'
+import AppProgrammatic from './components/AppProgrammatic/AppProgrammatic.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
     AppPerson,
+    AppProgrammatic,
   },
 })
 </script>
 
-<style>
-#app {
+<style scoped>
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.app > :not(:first-child) {
+  margin-block-start: 1rem;
 }
 </style>
---
+++ src/components/AppProgrammatic/AppProgrammatic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Programmatically</h1>
+    <div ref="target"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppPerson from '../AppPerson/AppPerson.vue'
+
+export default Vue.extend({
+  name: 'AppProgrammatic',
+
+  mounted() {
+    const vm = new AppPerson({
+      propsData: { firstName: 'Programmatically', lastName: 'Created', age: 1 },
+    })
+    const target = this.$refs.target as HTMLElement
+    vm.$mount(target)
+  },
+})
+</script>
