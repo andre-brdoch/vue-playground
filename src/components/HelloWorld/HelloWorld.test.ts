d4a25e64c5f75b136607be2c139877f850b62c71
andre-brdoch <andrebrdoch@yhahoo.de>
2023-03-09 10:03:55 +0100

    test(HelloWorld): Test Vue instance directly

--- src/components/HelloWorld/HelloWorld.test.ts
+++ src/components/HelloWorld/HelloWorld.test.ts
@@ -1,24 +1,30 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount, mount, Wrapper } from '@vue/test-utils'
 import HelloWorld from './HelloWorld.vue'
+import Vue from 'vue'
+
+// Sadly needed, since outside of the .vue file, TS can not see the proper types
+// of the component.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Component = Vue & { [key: string]: any }
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper: Wrapper<Component> = shallowMount(HelloWorld, {
       propsData: { msg },
     })
     expect(wrapper.text()).toMatch(msg)
   })
-  it('can be tested directly on the instance', () => {
+  it('can be tested directly on the instance', async () => {
     const msg = 'instance test'
-    const wrapper = mount(HelloWorld, {
+    const wrapper: Wrapper<Component> = mount(HelloWorld, {
       propsData: { msg },
     })
-
-    // @ts-ignore
     expect(wrapper.vm.msg).toBe(msg)
-    expect(wrapper.vm.$data.someData).toBe(true)
-    // @ts-ignore
     expect(wrapper.vm.msgLong).toBe(`${msg} LONG`)
+    expect(wrapper.vm.someData).toBe(true)
+    const newMsg = 'hi'
+    await wrapper.setProps({ msg: newMsg })
+    expect(wrapper.vm.msgLong).toBe(`${newMsg} LONG`)
   })
 })
